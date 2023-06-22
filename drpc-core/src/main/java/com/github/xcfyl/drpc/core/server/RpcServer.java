979f0b928d15d726305c0c907b7f942a09e0da4c
xcfyl <aiker67@163.com>
2023-06-22 11:00:13 +0800

    编写了rpc服务端，支持本地服务注册

--- drpc-core/src/main/java/com/github/xcfyl/drpc/core/server/RpcServer.java
+++ drpc-core/src/main/java/com/github/xcfyl/drpc/core/server/RpcServer.java
@@ -1,5 +1,15 @@
 package com.github.xcfyl.drpc.core.server;
 
+import com.github.xcfyl.drpc.core.common.RpcTransferProtocolDecoder;
+import com.github.xcfyl.drpc.core.common.RpcTransferProtocolEncoder;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import static com.github.xcfyl.drpc.core.server.RpcServerLocalCache.SERVICE_PROVIDER_MAP;
+
 /**
  * rpc服务端
  *
@@ -7,4 +17,50 @@ package com.github.xcfyl.drpc.core.server;
  * @date create at 2023/6/22 10:30
  */
 public class RpcServer {
+    private final RpcServerConfig config;
+
+    public RpcServer(RpcServerConfig config) {
+        this.config = config;
+    }
+
+    /**
+     * 启动rpc服务端
+     *
+     * @throws Exception
+     */
+    public void start() throws Exception {
+        ServerBootstrap bootstrap = new ServerBootstrap()
+                .group(new NioEventLoopGroup(), new NioEventLoopGroup())
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .option(ChannelOption.SO_BACKLOG, 1024)
+                .option(ChannelOption.SO_SNDBUF, 16 * 1024)
+                .option(ChannelOption.SO_RCVBUF, 16 * 1024)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel channel) throws Exception {
+                        channel.pipeline().addLast(new RpcTransferProtocolEncoder());
+                        channel.pipeline().addLast(new RpcTransferProtocolDecoder(config));
+                    }
+                });
+        bootstrap.bind(config.getPort()).sync();
+    }
+
+    /**
+     * 注册服务
+     *
+     * @param service
+     */
+    public void registerService(Object service) {
+        if (service.getClass().getInterfaces().length == 0) {
+            throw new RuntimeException("服务没有实现任何接口");
+        }
+        Class<?>[] interfaces = service.getClass().getInterfaces();
+        if (interfaces.length > 1) {
+            throw new RuntimeException("服务只能实现一个接口");
+        }
+        Class<?> interfaceClass = interfaces[0];
+        SERVICE_PROVIDER_MAP.put(interfaceClass.getName(), service);
+    }
 }
