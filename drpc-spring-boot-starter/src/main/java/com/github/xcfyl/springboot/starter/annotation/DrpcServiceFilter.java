83d6854b40ab38c9e45335aed4aca05e340e974a
xcfyl <aiker67@163.com>
2023-07-16 17:09:48 +0800

    fix

--- drpc-spring-boot-starter/src/main/java/com/github/xcfyl/springboot/starter/annotation/DrpcServiceFilter.java
+++ drpc-spring-boot-starter/src/main/java/com/github/xcfyl/springboot/starter/annotation/DrpcServiceFilter.java
@@ -4,5 +4,5 @@ package com.github.xcfyl.springboot.starter.annotation;
  * @author 西城风雨楼
  * @date create at 2023/7/16 17:05
  */
-public class DrpcServiceFilter {
+public @interface DrpcServiceFilter {
 }
